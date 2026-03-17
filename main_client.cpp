2beacf89e73318ce2abcef8d9c2e27b4e6eead00
ZipiRo <ionutperdun777@gmail.com>
2026-03-17 22:55:05 +0200

    that should be for the client

--- main_client.cpp
+++ main_client.cpp
@@ -178,6 +178,12 @@ int main()
         game_state = gamestate_packet.game_state;
     }
 
+    if(game_state.game_phase == GamePhase::GAMEOVER)
+    {
+        system("cls");
+        std::cout << "PLAYER: " << game_state.winner << " IS THE WINNER WITH " << game_state.player[game_state.winner].hit_count << " HIT\\S\n";
+    }
+
     client.Close();
 
     return 0;
